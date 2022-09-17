b377f88e409b64f18435b3143c8fbfbdb6516527
Tatyana <99961462+TatyanaDaviduk@users.noreply.github.com>
2022-09-17 18:36:58 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ const swiper = new Swiper('.swiper', {
   
 
   });
+
+
+
+  (function () {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('tab-active');
+        });
+        this.classList.add('tab-active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
+}());
